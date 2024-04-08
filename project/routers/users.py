3c61d8d16ca441f7ea992adb45983c9b4beb7388
joelminaya <joel.minaya@driv.in>
2024-04-08 00:45:50 -0500

    add login and cookies

--- project/routers/users.py
+++ project/routers/users.py
@@ -1,10 +1,15 @@
+from typing import List
+from fastapi import Response
+from fastapi import Cookie
 from fastapi import HTTPException,APIRouter
+
+from fastapi.security import HTTPBasicCredentials
 
 from ..database import User
 
 from ..schemas import UserRequestModel
 from ..schemas import UserResponseModel
-
+from ..schemas import ReviewResponseModel
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -18,3 +23,28 @@ async def create_user(user: UserRequestModel):
 
     user = User.create(username=user.username, password=hashed_password)
     return UserResponseModel(id=user.id, username=user.username)
+
+
+@router.post("/login", response_model=UserResponseModel)
+async def login_user(credentials: HTTPBasicCredentials, response: Response):
+    user = User.select().where(User.username == credentials.username).first()
+
+    if user == None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    if user.password != User.create_password_hash(credentials.password):
+        raise HTTPException(status_code=401, detail="Invalid password")
+    
+    response.set_cookie(key="user_id", value=str(user.id))
+
+    return user
+
+
+@router.get("/reviews", response_model=List[ReviewResponseModel])
+async def get_user_reviews(user_id: int = Cookie(None)):
+    user = User.select().where(User.id == user_id).first()
+
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return [ user_review for user_review in user.reviews]
